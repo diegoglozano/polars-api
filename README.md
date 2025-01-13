0eca8d333b966ce9fba423d1a8a15e10ba35ec6c
Diego García Lozano <38294364+diegoglozano@users.noreply.github.com>
2025-01-13 02:32:48 +0000

    [feat] get/aget/post/apost

--- README.md
+++ README.md
@@ -11,6 +11,82 @@ Polars extension for dealing with REST APIs
 - **Github repository**: <https://github.com/diegoglozano/polars-api/>
 - **Documentation** <https://diegoglozano.github.io/polars-api/>
 
+## Installation
+
+```sh
+uv add polars-api
+```
+
+```sh
+poetry add polars-api
+```
+
+```sh
+pip install polars-api
+```
+
+## Usage
+
+Just import the library as `import polars_api` and the new `api` namespace will be available.
+
+In the following example:
+
+- We set a base URL using [jsonplaceholder](https://jsonplaceholder.typicode.com/) as a fake REST API
+- For each row, we generate a different body using a `struct` type
+- Finally, we call different methods for getting the data:
+  - `.api.get()`: sync GET
+  - `.api.aget()`: async GET
+  - `.api.post()`: sync POST
+  - `.api.apost()`: async POST
+
+These methods will return the result as a `string`, but with polars you can convert it easily in a struct and access its values using `.str.json_decode()` method.
+
+```python
+import polars as pl
+import polars_api
+
+
+BASE_URL = "https://jsonplaceholder.typicode.com/posts"
+df = (
+    pl
+    .DataFrame({
+        "url": [BASE_URL for _ in range(10)],
+    })
+    .with_columns(
+        pl
+        .struct(
+            title=pl.lit("foo"),
+            body=pl.lit("bar"),
+            userId=pl.arange(10),
+        )
+        .alias("body"),
+    )
+    .with_columns(
+        pl
+        .col("url")
+        .api.get()
+        .str.json_decode()
+        .alias("get"),
+        pl
+        .col("url")
+        .api.aget()
+        .str.json_decode()
+        .alias("aget"),
+        pl
+        .col("url")
+        .api.post(body=pl.col("body"))
+        .str.json_decode()
+        .alias("post"),
+        pl
+        .col("url")
+        .api.apost(body=pl.col("body"))
+        .str.json_decode()
+        .alias("apost"),
+    )
+)
+
+```
+
 ---
 
 Repository initiated with [fpgmaas/cookiecutter-uv](https://github.com/fpgmaas/cookiecutter-uv).
--- polars_api/api.py
+++ polars_api/api.py
@@ -21,15 +21,36 @@ class Api:
     @staticmethod
     def _post(url: str, body: str) -> Optional[str]:
         result = httpx.post(url, json=body)
-        if result.status_code == 200:
-            return result.text
-        else:
-            return None
+        return result.text
+
+    @staticmethod
+    async def _aget_one(url: str, params: str) -> str:
+        async with httpx.AsyncClient() as client:
+            r = await client.get(url, params=params)
+            return r.text
+
+    async def _aget_all(self, x, params):
+        return await asyncio.gather(*[self._aget_one(url, param) for url, param in zip(x, params)])
+
+    def _aget(self, x, params):
+        return pl.Series(asyncio.run(self._aget_all(x, params)))
+
+    @staticmethod
+    async def _apost_one(url: str, body: str) -> str:
+        async with httpx.AsyncClient() as client:
+            r = await client.post(url, json=body)
+            return r.text
+
+    async def _apost_all(self, x, body):
+        return await asyncio.gather(*[self._apost_one(url, _body) for url, _body in zip(x, body)])
+
+    def _apost(self, x, body):
+        return pl.Series(asyncio.run(self._apost_all(x, body)))
 
     def get(self, params: Optional[pl.Expr] = None) -> pl.Expr:
         if params is None:
             params = pl.lit(None)
-        return pl.struct([self._url.alias("url"), params.alias("params")]).map_elements(
+        return pl.struct(self._url.alias("url"), params.alias("params")).map_elements(
             lambda x: self._get(x["url"], params=x["params"]),
             return_dtype=pl.Utf8,
         )
@@ -37,19 +58,21 @@ class Api:
     def post(self, body: Optional[pl.Expr] = None) -> pl.Expr:
         if body is None:
             body = pl.lit(None)
-        return pl.struct([self._url.alias("url"), body.alias("body")]).map_elements(
+        return pl.struct(self._url.alias("url"), body.alias("body")).map_elements(
             lambda x: self._post(x["url"], body=x["body"]),
             return_dtype=pl.Utf8,
         )
 
-    @staticmethod
-    async def _aget(url: str) -> str:
-        async with httpx.AsyncClient() as client:
-            r = await client.get(url)
-            if r.status_code == 200:
-                return r.text
-            else:
-                return ""
+    def aget(self, params: Optional[pl.Expr] = None) -> pl.Expr:
+        if params is None:
+            params = pl.lit(None)
+        return pl.struct(self._url.alias("url"), params.alias("params")).map_batches(
+            lambda x: self._aget(x.struct.field("url"), params=x.struct.field("params"))
+        )
 
-    def aget(self) -> pl.Expr:
-        return self._url.map_batches(lambda x: (asyncio.gather(*[self._aget(u) for u in x])))
+    def apost(self, body: Optional[pl.Expr] = None) -> pl.Expr:
+        if body is None:
+            body = pl.lit(None)
+        return pl.struct(self._url.alias("url"), body.alias("body")).map_batches(
+            lambda x: self._apost(x.struct.field("url"), body=x.struct.field("body"))
+        )
